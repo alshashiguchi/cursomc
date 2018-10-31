7083db4e87295f8c087aa8da109c65d2ce0ed65f
André Hashiguchi <andre.hashiguchi@onclick.com.br>
2018-10-31 00:11:21 -0300

    Estado e cidade

---
+++ src/main/java/com/alshashiguchi/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.alshashiguchi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alshashiguchi.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
